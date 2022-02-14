a8f69fca4eb113a5633184293c890f2e31cfef01
Seun O Ogundele <71834585+whoiskekeanyway@users.noreply.github.com>
2022-02-14 03:18:41 +0200

    fixes

--- square.js
+++ square.js
@@ -82,3 +82,35 @@
 //   updateDots(currentDot, targetDot);
 //   hideShowArrows(slides, prevButton, nextButton, targetIndex);
 // });
+
+
+
+var i = 0; // Start Point
+var images = []; // Images Array
+var time = 3000; // Time Between Switch
+
+// Image List
+// images[0] = "images/containers.jpeg";
+// images[1] = "images/plane.jpeg";
+// images[2] = "images/trucks.jpeg";
+// 
+// // Change Image
+// function changeImg() {
+//   document.slide.src = images[i];
+// 
+//   // Check If Index Is Under Max
+//   if (i < images.length - 1) {
+//     // Add 1 to Index
+//     i++;
+//   } else {
+//     // Reset Back To O
+//     i = 0;
+//   }
+// 
+//   // Run function every x seconds
+//   setTimeout("changeImg()", time);
+// }
+// 
+// // Run function when page loads
+// window.onload = changeImg;
+// 
