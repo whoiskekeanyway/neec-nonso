455091026c6de179884d9320f0a2b4015816d084
Seun O Ogundele <71834585+whoiskekeanyway@users.noreply.github.com>
2022-05-16 10:17:55 +0200

    almost-done

--- scripts/exhibitions.js
+++ scripts/exhibitions.js
@@ -1,64 +1,42 @@
-
-// Extra Animation in and out
 gsap.from(".extra", {
   scrollTrigger: {
     trigger: ".items-container",
-    start: -50, // when the top of the trigger hits the top of the viewport
-    end: "+=300", // end after scrolling 100px beyond the start
-    // markers: true
+    start: -50,
+    end: "+=300",
     scrub: 1,
   },
   stagger: { amount: 0.5 },
   scale: 0.8,
   duration: 1,
   ease: "power1.inOut",
-});
-
-// Footer animation in and out
-
-gsap.from(".footer-container", {
-  scrollTrigger: {
-    trigger: ".items-container",
-    start: -50, // when the top of the trigger hits the top of the viewport
-    end: "+=300", // end after scrolling 100px beyond the start
-    // markers: true
-    scrub: 1,
-  },
-  stagger: { amount: 0.5 },
-  scale: 0.5,
-  duration: 1,
-  ease: "power1.inOut",
-});
-
-// Main exhibition animation in and out
-gsap.from(".items-container", {
-  scrollTrigger: {
-    trigger: ".items-container",
-    start: -100, // when the top of the trigger hits the top of the viewport
-    end: "+=300", // end after scrolling 100px beyond the start
-    // markers: true
-    scrub: 1,
-  },
-  stagger: { amount: 0.5 },
-  scale: 0.9,
-  duration: 1,
-  ease: "power1.inOut",
-});
-
-// Timeline animation of the exhibition content on the whole page
-let tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".items-container",
-    //   markers: true,
-  },
-});
-
+}),
+  gsap.from(".footer-container", {
+    scrollTrigger: {
+      trigger: ".items-container",
+      start: -50,
+      end: "+=300",
+      scrub: 1,
+    },
+    stagger: { amount: 0.5 },
+    scale: 0.5,
+    duration: 1,
+    ease: "power1.inOut",
+  }),
+  gsap.from(".items-container", {
+    scrollTrigger: {
+      trigger: ".items-container",
+      start: -100,
+      end: "+=300",
+      scrub: 1,
+    },
+    stagger: { amount: 0.5 },
+    scale: 0.9,
+    duration: 1,
+    ease: "power1.inOut",
+  });
+let tl = gsap.timeline({ scrollTrigger: { trigger: ".items-container" } });
 tl.from(".items-container", { x: -200, opacity: 0.5, duration: 1.5 })
   .from(".App-logo", { y: -200, opacity: 0.5, duration: 1.5 })
   .from(".list-item", { y: -200, opacity: 0.5, duration: 1.5 })
   .from(".extra", { x: 200, opacity: 0, duration: 1.5 })
   .from(".copyright", { y: 10, opacity: 0, duration: 1.5 });
-
-
-
-
---
+++ scripts/projects.js
@@ -0,0 +1,99 @@
+const track = document.querySelector(".carousel-track"),
+  slides = Array.from(track.children),
+  nextButton = document.querySelector(".carousel--button-right"),
+  prevButton = document.querySelector(".carousel--button-left"),
+  dotsNav = document.querySelector(".carousel-nav"),
+  dots = Array.from(dotsNav.children),
+  slideWidth = slides[0].getBoundingClientRect().width,
+  setSlidePosition = (e, t) => {
+    e.style.left = slideWidth * t + "px";
+  };
+slides.forEach(setSlidePosition);
+const moveToSlide = (e, t, r) => {
+    (e.style.transform = "translateX(-" + r.style.left + ")"),
+      t.classList.remove("current-slide"),
+      r.classList.add("current-slide");
+  },
+  updateDots = (e, t) => {
+    e.classList.remove("current-slide"), t.classList.add("current-slide");
+  },
+  hideShowArrows = (e, t, r, s) => {
+    0 === s
+      ? (t.classList.add("is-hidden"), r.classList.remove("is-hidden"))
+      : s === e.length - 1
+      ? (t.classList.remove("is-hidden"), r.classList.add("is-hidden"))
+      : (t.classList.remove("is-hidden"), r.classList.remove("is-hidden"));
+  };
+prevButton.addEventListener("click", (e) => {
+  const t = track.querySelector(".current-slide"),
+    r = t.previousElementSibling,
+    s = dotsNav.querySelector(".current-slide"),
+    i = s.previousElementSibling,
+    o = slides.findIndex((e) => e === r);
+  moveToSlide(track, t, r),
+    updateDots(s, i),
+    hideShowArrows(slides, prevButton, nextButton, o);
+}),
+  nextButton.addEventListener("click", (e) => {
+    const t = track.querySelector(".current-slide"),
+      r = t.nextElementSibling,
+      s = dotsNav.querySelector(".current-slide"),
+      i = s.nextElementSibling,
+      o = slides.findIndex((e) => e === r);
+    moveToSlide(track, t, r),
+      updateDots(s, i),
+      hideShowArrows(slides, prevButton, nextButton, o);
+  }),
+  dotsNav.addEventListener("click", (e) => {
+    const t = e.target.closest("button");
+    if (!t) return;
+    const r = track.querySelector(".current-slide"),
+      s = dotsNav.querySelector(".current-slide"),
+      i = dots.findIndex((e) => e === t),
+      o = slides[i];
+    moveToSlide(track, r, o),
+      updateDots(s, t),
+      hideShowArrows(slides, prevButton, nextButton, i);
+  }),
+  gsap.from(".extra", {
+    scrollTrigger: {
+      trigger: ".main-exhibition",
+      start: -50,
+      end: "+=300",
+      scrub: 1,
+    },
+    stagger: { amount: 0.5 },
+    scale: 0.8,
+    duration: 1,
+  });
+let tl = gsap.timeline({ scrollTrigger: { trigger: ".boy-fraternity-title" } });
+tl
+  .from("main", { x: -200, opacity: 0.5, duration: 1.5 })
+  .from(".App-logo", { y: -200, opacity: 0.5, duration: 1.5 })
+  .from(".list-item", { y: -200, opacity: 0.5, duration: 1.5 })
+  .from(".extra", { x: 200, opacity: 0, duration: 1.5 })
+  .from(".copyright", { y: 10, opacity: 0, duration: 1.5 }),
+  gsap.from(".footer-container", {
+    scrollTrigger: {
+      trigger: ".main-exhibition",
+      start: "top top",
+      end: "+=300",
+      scrub: 1,
+    },
+    stagger: { amount: 0.5 },
+    scale: 0.6,
+    duration: 1,
+    ease: "power1.inOut",
+  }),
+  gsap.from(".thelastburial-paragraph", {
+    scrollTrigger: {
+      trigger: ".boy-fraternity-div",
+      start: -50,
+      end: "+=300",
+      scrub: 1,
+    },
+    stagger: { amount: 0.5 },
+    scale: 0.95,
+    duration: 1,
+    ease: "power1.inOut",
+  });
